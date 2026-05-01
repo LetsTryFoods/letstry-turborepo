e655e0e460ebcc4ada538978cb56e786f442c104
Lets Try Tech Team <tech@letstryfoods.com>
2026-05-01 16:35:37 +0530

    update-notification

--- apps/frontend/src/components/bestseller/bestseller-carousel.tsx
+++ apps/frontend/src/components/bestseller/bestseller-carousel.tsx
@@ -4,6 +4,8 @@ import { BestsellerCarouselClient } from "./bestseller-carousel-client";
 export const BestsellerCarousel = async () => {
   const products = await getBestsellerProducts("best-selling", 20);
 
+  
+
   if (products.length === 0) {
     return null;
   }
