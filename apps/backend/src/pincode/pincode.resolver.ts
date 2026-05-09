af5fdf7750188e33950fe9747c3b1f0fdf33c7a8
a-s-t-e-y-a <krishnoit100@gmail.com>
2026-05-09 16:31:49 +0530

    fix: make pincode endpoints public to resolve 401 Unauthorized

--- apps/backend/src/pincode/pincode.resolver.ts
+++ apps/backend/src/pincode/pincode.resolver.ts
@@ -1,11 +1,13 @@
 import { Resolver, Query, Args, Mutation } from '@nestjs/graphql';
 import { PincodeService } from './pincode.service';
 import { PincodeInput, PincodeServiceabilityResult } from './dto/pincode.input';
+import { Public } from '../common/decorators/public.decorator';
 
 @Resolver()
 export class PincodeResolver {
   constructor(private readonly pincodeService: PincodeService) {}
 
+  @Public()
   @Query(() => PincodeServiceabilityResult)
   async checkPincodeServiceability(
     @Args('pincode') pincode: string,
@@ -13,6 +15,7 @@ export class PincodeResolver {
     return this.pincodeService.checkServiceability(pincode);
   }
 
+  @Public()
   @Mutation(() => Number)
   async bulkUpsertPincodes(
     @Args('pincodes', { type: () => [PincodeInput] }) pincodes: PincodeInput[],
